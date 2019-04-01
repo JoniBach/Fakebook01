cdcc60213819e776552894f059fc988103330224
James Crook <james@jamescrook.uk>
2019-04-01 10:36:04 +0100

    Initial commit

---
+++ Fakebook01.js
@@ -0,0 +1,30 @@
+//generative collage of zuckerberg by jamescrook.uk
+
+function preload() {
+  // preload() runs once
+  pimg = loadImage("zucc1.jpg");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+
+  // setup() runs once
+}
+function draw() {
+  // draw() loops forever, until stopped
+
+  for (let i = 0; i < 100; i++) {
+    let r = random(10, 30);
+    let e = random(0, 30);
+
+
+
+    x = random(width);
+    y = random(height);
+
+    c = pimg.get(int(x), int(y));
+    fill(c);
+rect(x, y, r, r, e, e, e, e);
+
+  }
+} 
---
+++ fakeJavascript.js
@@ -0,0 +1,35 @@
+function myFunctionB() {
+  var x = document.getElementById("myDIVb");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionA() {
+  var x = document.getElementById("myDIVa");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionC() {
+  var x = document.getElementById("myDIVc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myAnimationA() {
+  let sketch = function(p) {
+    p.setup = function(){
+      p.createCanvas(100, 100);
+      p.background(0);
+    }
+  };
+  let node = document.createElement('div');
+  new p5(sketch, node);
+  window.document.getElementsByTagName('body')[0].appendChild(node);
+}
